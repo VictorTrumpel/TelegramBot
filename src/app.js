c39c1647fb327e1fc3cfb0be0c70e04c329990ff
Victor Trumpel <victor.trumpel@gmail.com>
2023-02-25 16:26:14 +0300

    add loader for request

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 const { Configuration, OpenAIApi } = require('openai');
 const { Telegraf } = require('telegraf');
 
-console.log('Telegraf :>> ', Telegraf);
-
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 });
@@ -23,14 +21,30 @@ const askChatGPT = async (message) => {
   return completion.data.choices[0].text
 }
 
+let isPending = false
+
+let interval = null
+
 bot.on('message', async (ctx) => {
   const { text } = ctx.update.message
 
+  isPending = true
+
   ctx.sendChatAction('typing')
+
+  interval = setInterval(() => {
+    console.log('isPending :>> ', isPending);
+    if (isPending) {
+      ctx.sendChatAction('typing')
+    } else {
+      clearInterval(interval)
+    }
+  }, 1000)
 
   const response = await askChatGPT(text);
 
-  ctx.sendChatAction('typing')
+  isPending = false
+  clearInterval(interval)
 
   await ctx.reply(response)
 });
