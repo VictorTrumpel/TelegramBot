859d198cb799964acd99dfad39c2c63ccadc2efd
Victor Trumpel <victor.trumpel@gmail.com>
2023-04-12 23:17:35 +0300

    add payment server

--- src/app.js
+++ src/app.js
@@ -2,15 +2,16 @@
 
 require("dotenv").config();
 const { Telegraf } = require('telegraf');
+const express = require('express');
 const Client = require('./client/Client');
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
 
+const paymentServer = express()
+
 bot.on('pre_checkout_query', (ctx) => ctx.answerPreCheckoutQuery(true))
 
 bot.on('message', async (ctx) => {
-
-
   return ctx.reply(
     'random example',
     {
@@ -29,5 +30,15 @@ bot.on('message', async (ctx) => {
 
   // new Client(ctx)
 })
+
+
+paymentServer.get('/', (req, res) => {
+ 
+  res.json('Hello world')
+})
+
+paymentServer.listen(9999, () => {
+  console.log('Сервер оплаты развернут на 9999 порту');
+});
 
 bot.launch()
